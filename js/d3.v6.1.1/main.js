98bd0e83db83ddfd05fbcfb567bc6d6e1d68f34c
tchen2 <chen.ter@husky.neu.edu>
2022-10-26 14:09:18 -0400

    finished but feel free to override

--- js/d3.v6.1.1/main.js
+++ js/d3.v6.1.1/main.js
@@ -1,6 +1,11 @@
 const FRAME_HEIGHT = 450;
 const FRAME_WIDTH = 450; 
 const MARGINS = {left: 60, right: 60, top: 60, bottom: 60};
+let bar_chart;
+let xCoord1 ='Sepal_Length';
+let yCoord1 = 'Petal_Length';
+let xCoord2 = "Sepal_Width";
+let yCoord2 = 'Petal_Width';
 
 // vis dimensions
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.left - MARGINS.right;
@@ -33,10 +38,10 @@ d3.csv("data/iris.csv").then((data) => {
 // building plot for Sepal_Length vs Petal_Length
     console.log(data);
     // find max X
-    const MAX_X = d3.max(data, (d) => { return parseInt(d.Sepal_Length); });
+    const MAX_X = d3.max(data, (d) => { return parseInt(d[xCoord1]); });
 
     // find max Y
-    const MAX_Y = d3.max(data, (d) => { return parseInt(d.Petal_Length); });
+    const MAX_Y = d3.max(data, (d) => { return parseInt(d[yCoord1]); });
 
     // Define scale functions that maps our data values 
     // (domain) to pixel values (range)
@@ -52,12 +57,12 @@ d3.csv("data/iris.csv").then((data) => {
 
     
     // Use X_SCALE to plot our points
-    let circles = FRAME1.selectAll("points")  
+    let first_scatter = FRAME1.selectAll("points")  
             .data(data) // passed from .then  
             .enter()       
             .append("circle")  
-                .attr("cx", (d) => { return (X_SCALE(d.Sepal_Length) );}) 
-                .attr("cy", (d) => { return (Y_SCALE(d.Petal_Length));}) 
+                .attr("cx", (d) => { return (X_SCALE(d[xCoord1]) );}) 
+                .attr("cy", (d) => { return (Y_SCALE(d[yCoord1]));}) 
                 .attr("r", 5)
                 .attr("class", (d) =>{ return d.Species; });
 
@@ -77,10 +82,10 @@ d3.csv("data/iris.csv").then((data) => {
 // building plot for Sepal_Width vs Petal_Width
 
     // find max X
-    const MAX_X2 = d3.max(data, (d) => { return parseInt(d.Sepal_Width); });
+    const MAX_X2 = d3.max(data, (d) => { return parseInt(d[xCoord2]); });
 
     // find max Y
-    const MAX_Y2 = d3.max(data, (d) => { return parseInt(d.Petal_Width); });
+    const MAX_Y2 = d3.max(data, (d) => { return parseInt(d[yCoord2]); });
 
     // Define scale functions that maps our data values 
     // (domain) to pixel values (range)
@@ -96,12 +101,12 @@ d3.csv("data/iris.csv").then((data) => {
 
     
     // Use X_SCALE to plot our points
-    FRAME2.selectAll("points")  
+    let second_scatter = FRAME2.selectAll("points")  
             .data(data) // passed from .then  
             .enter()       
             .append("circle")  
-                .attr("cx", (d) => { return (X_SCALE2(d.Sepal_Width) +MARGINS.left);}) 
-                .attr("cy", (d) => { return (Y_SCALE2(d.Petal_Width)) + MARGINS.bottom;}) 
+                .attr("cx", (d) => { return (X_SCALE2(d[xCoord2]) +MARGINS.left);}) 
+                .attr("cy", (d) => { return (Y_SCALE2(d[yCoord2])) + MARGINS.top;}) 
                 .attr("r", 5)
                 .attr("class", (d) =>{ return d.Species; });
 
@@ -118,7 +123,48 @@ d3.csv("data/iris.csv").then((data) => {
     .call(d3.axisLeft(Y_SCALE2))
             .attr('font-size', '10px');
 
-   
+    
+// initialize brush and event handler
+    const brush = d3.brush()
+                        .extent([[MARGINS.left,MARGINS.top], [FRAME_WIDTH,(FRAME_HEIGHT - MARGINS.bottom)]])
+                        .on("brush", brush_charts);
+    FRAME2.call(brush);
+
+    //implement brushing funcitonality
+    function brush_charts(e) {
+
+        // set to track highlighting in bar chart
+        let selected = new Set();
+
+        second_scatter.classed('brush', (d) => {
+        let brushed = isBrushed(e.selection, (MARGINS.left + X_SCALE2(d[xCoord2])), (MARGINS.top + Y_SCALE2(d[yCoord2])));
+
+        // if points in the second scatter are brushed, add to selected to reflect in bar chart
+        if (brushed) {
+                selected.add(d.Species);
+        }
+        return brushed});
+
+        // highlights left plot
+        first_scatter.classed("brush", (d) => isBrushed(e.selection, (MARGINS.left + X_SCALE2(d[xCoord2])), (MARGINS.top + Y_SCALE2(d[yCoord2]))));
+
+        // highlights bars if the corresponding points are selected
+        bar_chart.classed("brush", (d) => {
+                return selected.has(d.species);})
+
+        };
+// };
+
+    // function to determine points within brushed region
+    function isBrushed(brush_coords, cx, cy) {
+        if(brush_coords == null) 
+                return;
+        const x0 = brush_coords[0][0];
+        const x1 = brush_coords[1][0];
+        const y0 = brush_coords[0][1];
+        const y1 = brush_coords[1][1];
+        return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;    
+    };
 
 });
 
@@ -142,7 +188,7 @@ d3.csv("data/species.csv").then((data) => {
     
     
         // plot bars using x and y scale
-        var bars = FRAME3.selectAll(".bar")
+        bar_chart = FRAME3.selectAll(".bar")
              .data(data)
              .enter()
              .append("rect")
@@ -165,5 +211,4 @@ d3.csv("data/species.csv").then((data) => {
                 "," + (MARGINS.bottom) +")") 
         .call(d3.axisLeft(Y_SCALE).ticks(5))
                 .attr('font-size', '10px'); 
-
         });
